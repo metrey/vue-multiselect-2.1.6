01843a91d4752f4f31bc002071f4a92216d3502f
Pongsametrey Sok <metreysk@gmail.com>
2019-10-03 16:00:28 +0700

    custom from release 2.1.6

---
+++ documentation/partials/examples/SingleSelectObject.vue
@@ -0,0 +1,54 @@
+<template lang="pug">
+div
+  label.typo__label Single select / dropdown
+  multiselect(
+    v-model="value",
+    deselect-label="Can't remove this value",
+    track-by="name",
+    label="name",
+    placeholder="Select one",
+    :options="options",
+    :searchable="false",
+    :allow-empty="false"
+    :showDetail="true"
+    @viewDetail="viewIt"
+  )
+    template(slot="singleLabel", slot-scope="{ option }")
+      strong {{ option.name }}
+      |  is written in
+      strong   {{ option.language }}
+  pre.language-json
+    code.
+      {{ value  }}
+
+</template>
+
+<script>
+import Multiselect from 'vue-multiselect'
+
+export default {
+  components: {
+    Multiselect
+  },
+  data () {
+    return {
+      value: null,
+      options: [
+        { name: 'Vue.js', language: 'JavaScript' },
+        { name: 'Rails', language: 'Ruby' },
+        { name: 'Sinatra', language: 'Ruby' },
+        { name: 'Laravel', language: 'PHP', $isDisabled: true },
+        { name: 'Phoenix', language: 'Elixir' }
+      ]
+    }
+  },
+  methods: {
+    viewIt(event) {
+      alert(JSON.stringify(event))
+    }
+  }
+}
+</script>
+
+<style lang="css">
+</style>
---
+++ documentation/partials/examples/SingleSelectSearch.vue
@@ -0,0 +1,50 @@
+<template lang="pug">
+div
+  label.typo__label Select with search
+  multiselect(
+    v-model="value",
+    :options="options",
+    :custom-label="nameWithLang"
+    placeholder="Select one",
+    label="name",
+    track-by="name"
+    :showDetail="true"
+    @viewDetail="viewIt"
+  )
+  pre.language-json
+    code.
+      {{ value  }}
+</template>
+
+<script>
+import Multiselect from 'vue-multiselect'
+
+export default {
+  components: {
+    Multiselect
+  },
+  data () {
+    return {
+      value: { name: 'Vue.js', language: 'JavaScript' },
+      options: [
+        { name: 'Vue.js', language: 'JavaScript' },
+        { name: 'Rails', language: 'Ruby' },
+        { name: 'Sinatra', language: 'Ruby' },
+        { name: 'Laravel', language: 'PHP' },
+        { name: 'Phoenix', language: 'Elixir' }
+      ]
+    }
+  },
+  methods: {
+    nameWithLang ({ name, language }) {
+      return `${name} — [${language}]`
+    },
+    viewIt(event) {
+      alert(JSON.stringify(event))
+    }
+  }
+}
+</script>
+
+<style lang="css">
+</style>
